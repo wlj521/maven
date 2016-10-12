605b2003bbffa308526482c7b170888f7ec64cef
wlj521 <512372507@qq.com>
2016-10-12 10:19:36 +0800

    新建项目

---
+++ first/first-facade/src/main/java/com/wlj/first/facade/App.java
@@ -0,0 +1,13 @@
+package com.wlj.first.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
